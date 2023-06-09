2d032e17a7e6f754b2374e7c6b029a0c67e76e52
Sandro Lain <sandrolain@outlook.com>
2023-06-09 21:57:50 +0200

    Added usage of Shoelace custom elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import {SlDrawer} from '@shoelace-style/shoelace';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [
+    './app.component.scss'
+  ]
 })
 export class AppComponent {
   title = 'wc-in-angular';
+
+  // use @ViewChild to get a reference to the #drawer element within component template
+  @ViewChild('drawer')
+  drawer?: ElementRef<SlDrawer>;
+
+  showDrawer() {
+    // use nativeElement to access Shoelace components
+    console.log(this.drawer, this.drawer?.nativeElement.constructor)
+    this.drawer?.nativeElement.show();
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import '@shoelace-style/shoelace';
+// import '@shoelace-style/shoelace/dist/themes/light.css';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.4.0/dist/');
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
